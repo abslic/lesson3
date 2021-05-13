2f2d36060194a35abf1d2a1c1731f662f1fdced1
Алексей Гришин <83308525+abslic@users.noreply.github.com>
2021-05-13 04:01:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    static int CalcOddPositiveSum(int sum)
+    {
+        Console.Write("Введите числа (0 для выхода из программы): ");
+        int num = int.Parse(Console.ReadLine());
+        if (num == 0)
+            return sum;
+        if (num > 0 && num % 2 == 1)
+            return CalcOddPositiveSum(num + sum);
+        return CalcOddPositiveSum(sum);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Сумма положительных чисел: {0}\n", CalcOddPositiveSum(0));
+        Console.ReadKey();
+    }
+}
